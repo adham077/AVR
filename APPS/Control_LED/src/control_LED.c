0d0bec4d4fee477c521a0b6aeb5eda697eabe561
Adham Walaa <adham.walaa@gmail.com>
2024-12-25 04:13:54 +0200

    added configuration for both pins and ports

--- APPS/Control_LED/src/control_LED.c
+++ APPS/Control_LED/src/control_LED.c
@@ -3,56 +3,28 @@
 void delay(void);
 
 int main(void){
-    
-    MDIO_enuSetPinConfigration(MDIO_enu_PORTA,MDIO_enu_PIN0,MDIO_enu_OUTPUT);
-    MDIO_enuSetPinConfigration(MDIO_enu_PORTA,MDIO_enu_PIN1,MDIO_enu_OUTPUT);
-    MDIO_enuSetPinConfigration(MDIO_enu_PORTA,MDIO_enu_PIN2,MDIO_enu_OUTPUT);
-    MDIO_enuSetPinConfigration(MDIO_enu_PORTB,MDIO_enu_PIN3,MDIO_enu_INPUT_PULLUP);
-    MDIO_enuSetPinValue(MDIO_enu_PORTA,MDIO_enu_PIN0,MDIO_enu_LOW);
-    MDIO_enuSetPinValue(MDIO_enu_PORTA,MDIO_enu_PIN1,MDIO_enu_LOW);
-    MDIO_enuSetPinValue(MDIO_enu_PORTA,MDIO_enu_PIN2,MDIO_enu_LOW);
-    
-    uint8_t button_state = MDIO_enu_HIGH;
-    uint8_t led_state = 0;
-    while(1){
-        
-        MDIO_enuGetPinValue(MDIO_enu_PORTA,MDIO_enu_PIN3,&button_state);
-        if(button_state==MDIO_enu_LOW){
-            delay();
-            MDIO_enuGetPinValue(MDIO_enu_PORTA,MDIO_enu_PIN3,&button_state);
-            if(button_state==MDIO_enu_LOW){
-                led_state++;
-                led_state %= 3;
-            }   
-            while(button_state==MDIO_enu_LOW)
-                MDIO_enuGetPinValue(MDIO_enu_PORTA,MDIO_enu_PIN3,&button_state);
-        }
+    MDIO_vInit();
+    uint8_t button_state = 0;
+    uint8_t prev_button_state = 1;  
+    uint8_t mode = 0;
 
-        switch(led_state){
-            case 0:
-                MDIO_enuSetPinConfigration(MDIO_enu_PORTA,MDIO_enu_PIN0,MDIO_enu_HIGH);
-                MDIO_enuSetPinConfigration(MDIO_enu_PORTA,MDIO_enu_PIN1,MDIO_enu_LOW);
-                MDIO_enuSetPinConfigration(MDIO_enu_PORTA,MDIO_enu_PIN2,MDIO_enu_LOW);
-                break;
-            case 1:
-                MDIO_enuSetPinConfigration(MDIO_enu_PORTA,MDIO_enu_PIN0,MDIO_enu_LOW);
-                MDIO_enuSetPinConfigration(MDIO_enu_PORTA,MDIO_enu_PIN1,MDIO_enu_HIGH);
-                MDIO_enuSetPinConfigration(MDIO_enu_PORTA,MDIO_enu_PIN2,MDIO_enu_LOW);
-                break;
-            case 2:
-                MDIO_enuSetPinConfigration(MDIO_enu_PORTA,MDIO_enu_PIN0,MDIO_enu_LOW);
-                MDIO_enuSetPinConfigration(MDIO_enu_PORTA,MDIO_enu_PIN1,MDIO_enu_LOW);
-                MDIO_enuSetPinConfigration(MDIO_enu_PORTA,MDIO_enu_PIN2,MDIO_enu_HIGH);
-                break;
-            default:
-                break;
+    MDIO_enuGetPinValue(MDIO_enu_PORTA, MDIO_enu_PIN0, &button_state);
+    MDIO_enuSetPinValue(MDIO_enu_PORTA, MDIO_enu_PIN3, mode);        
+
+    while(1){
+        MDIO_enuGetPinValue(MDIO_enu_PORTA, MDIO_enu_PIN0, &button_state);  
+        if(button_state == MDIO_enu_LOW && prev_button_state == MDIO_enu_HIGH) {  
+            delay();              
+            mode ^= 0x01;
         }
+        MDIO_enuSetPinValue(MDIO_enu_PORTA, MDIO_enu_PIN3, mode);
+        prev_button_state = button_state;
     }
 
     return 0;
 }
 
 void delay(void){
-    int i=0;
-    for(;i<1000;i++);
+    int i = 0;
+    for(; i < 1000; i++);
 }
