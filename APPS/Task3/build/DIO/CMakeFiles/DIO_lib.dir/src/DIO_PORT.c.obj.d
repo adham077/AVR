9e80a2b27dbe8062b8adcbf869a28fb1068c471b
Adham Walaa <adham.walaa@gmail.com>
2024-12-30 01:08:09 +0200

    update

--- APPS/Task3/build/DIO/CMakeFiles/DIO_lib.dir/src/DIO_PORT.c.obj.d
+++
@@ -1,6 +0,0 @@
-DIO/CMakeFiles/DIO_lib.dir/src/DIO_PORT.c.obj:  \
- C:\ITI\ Intake-45\ 9\ Months\github_repos\AVR\COTS\MCAL\DIO\src\DIO_PORT.c \
-  C:\ITIINT~1\GITHUB~1\AVR\COTS\MCAL\DIO\inc/DIO_PORT.h \
-  C:\ITIINT~1\GITHUB~1\AVR\COTS\MCAL\DIO\inc/DIO_CFG.h \
-  C:\ITIINT~1\GITHUB~1\AVR\COTS\MCAL\DIO\..\..\STD_LIB/std_types.h \
-  C:\ITIINT~1\GITHUB~1\AVR\COTS\MCAL\DIO\..\..\STD_LIB/bit_math.h
